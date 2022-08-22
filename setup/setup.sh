437242dbceeba39476decb1fe5640056a0771d25
mpcarolin <mpcarolin@gmail.com>
2022-08-21 20:32:43 -0700

    add setup script

---
+++ setup/setup.sh
@@ -0,0 +1,6 @@
+# This file installs any dependencies needed for my neovim work environment.
+# It is backed up by the dotfiles repo
+
+# get nerd fonts
+brew tap homebrew/cask-fonts
+brew install --cask font-dejavu-sans-mono-nerd-font
