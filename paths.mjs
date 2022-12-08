8d21bc4e6630c495e59b676bec443716d5af5855
michael.carolin <michael.carolin@simpleviewinc.com>
2022-12-07 20:00:55 -0700

    Update setup - Add zx, alacritty, tmux, and neovim to setup - Remove qutebrowser references

--- paths.mjs
+++ paths.mjs
@@ -1,7 +1,6 @@
 export const paths = {
   alacritty: [ '~/.config/alacritty/alacritty.yml' ],
   nvim: [ '~/.config/nvim/init.vim', '~/.config/nvim/coc-settings.json' ],
-  qutebrowser: [ '~/.qutebrowser/config.py', '~/.qutebrowser/nord-qutebrowser.py' ],
   tmux: [ '~/.tmux.conf' ],
   setup: [ '~/setup.sh' ],
 }
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,16 @@
 # This file installs any dependencies needed for my neovim work environment.
 # It is backed up by the dotfiles repo
 
+brew install zx
+sudo chown -R $(whoami):staff /usr/local/lib/node_modules/
+
+# terminal
+brew install --cask alacritty
+brew install tmux
+
+# neovim
+brew install neovim
+
 # get nerd fonts
 brew tap homebrew/cask-fonts
 brew install --cask font-dejavu-sans-mono-nerd-font
